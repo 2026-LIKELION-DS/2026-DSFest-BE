44fff57561357db3b6d3fb2bf31e05faeb925466
b1nnnnid <suebenny2@naver.com>
2026-04-21 21:17:30 +0900

    Fix: DAY별 부스 조회 중복 에러 수정

--- src/main/java/com/ds/dsfest/domain/booth/repository/BoothOperatingDayRepository.java
+++ src/main/java/com/ds/dsfest/domain/booth/repository/BoothOperatingDayRepository.java
@@ -1,13 +1,11 @@
 package com.ds.dsfest.domain.booth.repository;
 
-import java.time.LocalTime;
-import java.util.List;
-
 import com.ds.dsfest.domain.booth.entity.BoothOperatingDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface BoothOperatingDayRepository extends JpaRepository<BoothOperatingDay, Long> {
@@ -17,7 +15,6 @@ public interface BoothOperatingDayRepository extends JpaRepository<BoothOperatin
           + "JOIN FETCH od.booth b "
           + "LEFT JOIN FETCH b.tags "
           + "LEFT JOIN FETCH b.boothTypes "
-          + "LEFT JOIN FETCH b.images "
           + "WHERE od.festivalDay = :festivalDay "
           + "ORDER BY b.boothNumber ASC")
   List<BoothOperatingDay> findAllByFestivalDayWithBooth(@Param("festivalDay") int festivalDay);
